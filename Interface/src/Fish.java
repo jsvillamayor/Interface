9ae921188a0ff95e69cda07b5fbfd19dfa23a424
jsvillamayor <78101377+jsvillamayor@users.noreply.github.com>
2022-03-30 12:16:39 -0700

    Add files via upload

---
+++ Interface/src/Fish.java
@@ -0,0 +1,15 @@
+
+public class Fish implements Prey, Predator{
+	
+	public void flee() {
+		
+		System.out.println("This fish is fleeing from a larger fish");
+		
+	}
+	public void hunt() {
+		
+		System.out.println("This fish is hunting smaller fish");
+		
+	}
+
+}
---
+++ Interface/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rabbit rabbit = new Rabbit();
+		rabbit.flee();
+		Hawk hawk = new Hawk();
+		hawk.hunt();
+		Fish fish = new Fish();
+		fish.flee();
+		fish.hunt();
+
+	}
+
+}
